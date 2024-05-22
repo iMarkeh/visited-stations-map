6cd465ccb3d8ff62fe7c3847274896dff67d402b
David Wheatley <david@davwheat.dev>
2024-05-22 18:57:10 +0100

    feat: sheet link

--- html/map.js
+++ html/map.js
@@ -13,6 +13,7 @@ const map = new maplibregl.Map({
     customAttribution: [
       `<a href="https://github.com/davwheat/visited-stations-map">View source code</a>`,
       `Inspired by <a href="https://github.com/t5r7/station-mapper">t5r7's station mapper</a>`,
+      `<a target="_blank" href="https://docs.google.com/spreadsheets/d/1ZRBE-9i4_WmMmO5h1pIMwrF1owW95Qc-ElLcmf0ct3g/edit">Data source</a>`,
       '© OpenStreetMap contributors',
     ],
   }),
